3aa2bef0fec4f24d1bb5955a081012a3cb060bcc
wemali05 <alfredwemali05@gmail.com>
2019-07-25 13:55:21 +0300

    laravel vue ecommerce

---
+++ app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use Notifiable, SoftDeletes, HasApiTokens;
+
+   
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    
+    
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function orders(){
+        return $this->hasMany(Order::class);
+    }
+    
+}
